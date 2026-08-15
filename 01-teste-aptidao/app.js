41571d4d2449406fafa513893f5f47082d6c5327
Lucas Carvalho <lucas.290sabino@gmail.com>
2026-08-15 11:53:40 -0300

    js finalizdo

---
+++ 01-teste-aptidao/app.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const botaoAvaliar = document.getElementById('avaliar')
+
+function avaliarTempo(){
+    const caixatempo = document.getElementById('tempo')
+    const caixaResultado = document.getElementById('resultado')
+    if (caixatempo.value <=14){
+        caixaResultado.textContent= "Apto"
+        caixaResultado.classList.add ('apto')
+        caixaResultado.classList.remove('nao-apto')
+    }else{
+        caixaResultado.textContent = "Não apto"
+        caixaResultado.classList.add  ('nao-apto')
+        caixaResultado.classList.remove('apto')
+    }
+
+}
+botaoAvaliar.addEventListener('click', avaliarTempo)
+
